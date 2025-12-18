ddcd96bfdf2487c3e674f29af56e21211468dce5
frbranda <frbranda@c2r5s2.42porto.com>
2025-12-18 14:59:07 +0000

    Cpp06 ex01 finished

--- CPP_06/ex01/Utils.hpp
+++ CPP_06/ex01/Utils.hpp
@@ -6,7 +6,7 @@
 /*   By: frbranda <frbranda@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 14:19:34 by frbranda          #+#    #+#             */
-/*   Updated: 2025/12/17 17:31:38 by frbranda         ###   ########.fr       */
+/*   Updated: 2025/12/18 14:49:46 by frbranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,12 @@
 
 #include <iostream>
 #include <string>
+#include "stdint.h"
 
 #include "Color.hpp"
 
-/* Class and identifier colors */
-#define CLASS_COLOR		BOLD_Y
-#define NAME_COLOR		RGB_BOLD(100, 150, 250)
+#define NUM_COLOR	RGB_BOLD(255, 165, 0)
 
-#define NUM_COLOR		RGB_BOLD(255, 165, 0)
-
-#define BOLD_O RGB_BOLD(220, 69, 0)
+#define BOLD_O		RGB_BOLD(220, 69, 0)
 
 #endif
--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -6,23 +6,57 @@
 /*   By: frbranda <frbranda@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 17:05:40 by frbranda          #+#    #+#             */
-/*   Updated: 2025/12/16 18:40:37 by frbranda         ###   ########.fr       */
+/*   Updated: 2025/12/18 14:57:01 by frbranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ScalarConverter.hpp"
+#include "Serializer.hpp"
 
-int main (int argc, char* argv[])
+void showData(const Data& d)
 {
-	if (argc != 2)
-	{
-		std::cerr << BOLD_R << "Error: Most have 1 argument!"
-					<< RST << std::endl;
-		return 1;
-	}
+	std::cout << "Name:    " << NUM_COLOR << d.name << RST << std::endl;
+	std::cout << "Number:  " << NUM_COLOR << d.num << RST << std::endl;
+	std::cout << "Status:  " << (d.status ? BOLD_G "true" : BOLD_R "false")
+				<< RST << std::endl;
+	std::cout << "Address: " << NUM_COLOR << &d << RST << std::endl;
+}
+
+int main ()
+{
+	Data d = {"Bacon", 25, true};
+
+	std::cout << BOLD_C << "=== Original Data ===" << RST << std::endl;
+	showData(d);
+
 	
-	ScalarConverter::convert(argv[1]);
+	uintptr_t serializeData = Serializer::serialize(&d);
+
+	std::cout << BOLD_C << "\n=== Serialized Data ===" << RST << std::endl;
+	std::cout << "Raw value (dec): " << NUM_COLOR << serializeData
+			  << RST << std::endl;
+	std::cout << "Raw value (hex): " << NUM_COLOR
+			  << std::showbase << std::hex << serializeData
+			  << std::noshowbase << std::dec << RST << std::endl;
+
+	Data* d2 = Serializer::deserialize(serializeData);
+
+	std::cout << BOLD_C << "\n=== Deserialized Data ===" << RST << std::endl;
+	showData(*d2);
+	
+	
+	*d2 = (Data){"CJ", 26, false};
+
+	d2->name = "CJ";
+	d2->num = 26;
+	d2->status = false;
+	
+	std::cout << BOLD_C << "\n\n=== Modified Data (via d2) ===" << RST << std::endl;
+	showData(*d2);
+	
+	std::cout << BOLD_C << "\n=== Original Data (d) After Modification ===" << RST << std::endl;
+	showData(d);
 	
 	return 0;
 }
+
 
