a8aa9cfa9ae98b4160965f55cfe5ce1645a9054d
frbranda <frbranda@c2r6s7.42porto.com>
2025-12-19 15:46:29 +0000

    Cpp07 ex00 updated

--- CPP_07/ex00/main.cpp
+++ CPP_07/ex00/main.cpp
@@ -6,47 +6,91 @@
 /*   By: frbranda <frbranda@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 17:05:40 by frbranda          #+#    #+#             */
-/*   Updated: 2025/12/18 18:37:59 by frbranda         ###   ########.fr       */
+/*   Updated: 2025/12/19 15:45:11 by frbranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
-void test1_Int()
+template <typename T>
+void runTest(T& a, T& b)
 {
-
+	::swap( a, b );
+	
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << std::endl;
 }
 
-void test2_Double()
+int main( void )
 {
+	int a = 2;
+	int b = 3;
+	
+	::runTest(a, b);
+	
 
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	
+	::runTest(c, d);
+
+	
+	double e = 4.2;
+	double f = 5.8;
+	
+	::runTest(e, f);
+
+
+	const int g = 1;
+	const int h = 6;
+	
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << std::endl;
+	
+	return 0;
 }
 
-void test3_String()
-{
-
-}
-
-int main ()
-{
-	int	a = 4;
-	int b = 2;
 
 
-	std::cout << "=== Before Swap ===" << std::endl;
-	std::cout << "a: " << a << std::endl;
-	std::cout << "b: " << b << std::endl;
+// void test1_Int()
+// {
+
+// }
+
+// void test2_Double()
+// {
+
+// }
+
+// void test3_String()
+// {
+
+// }
+
+// int main ()
+// {
+// 	int	a = 4;
+// 	int b = 2;
+
+
+// 	std::cout << "=== Before Swap ===" << std::endl;
+// 	std::cout << "a: " << a << std::endl;
+// 	std::cout << "b: " << b << std::endl;
 	
-	Whatever<int>::swap(a, b);
+// 	Whatever<int>::swap(a, b);
 
-	std::cout << "\n=== After Swap ===" << std::endl;
-	std::cout << "a: " << a << std::endl;
-	std::cout << "b: " << b << std::endl;
+// 	std::cout << "\n=== After Swap ===" << std::endl;
+// 	std::cout << "a: " << a << std::endl;
+// 	std::cout << "b: " << b << std::endl;
 	
 	
-	std::cout << "\nmin( a, b ) = " << Whatever<int>::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << Whatever<int>::max( a, b ) << std::endl;
+// 	std::cout << "\nmin( a, b ) = " << Whatever<int>::min( a, b ) << std::endl;
+// 	std::cout << "max( a, b ) = " << Whatever<int>::max( a, b ) << std::endl;
 
 	
-}
+// }
 
--- CPP_07/ex00/whatever.hpp
+++ CPP_07/ex00/whatever.hpp
@@ -6,45 +6,89 @@
 /*   By: frbranda <frbranda@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 17:36:33 by frbranda          #+#    #+#             */
-/*   Updated: 2025/12/18 18:35:23 by frbranda         ###   ########.fr       */
+/*   Updated: 2025/12/19 15:23:34 by frbranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
-#include "Utils.hpp"
+#include <iostream>
+#include <iomanip>
+#include <string>
 
 template <typename T>
-class Whatever
+void swap(T& a, T& b)
 {
-	public:
-		static void swap(T& a, T& b)
-		{
-			T temp = a;
-			a = b;
-			b = temp;
-		}
+	T temp = a;
+	a = b;
+	b = temp;
+}
 
-		static T& min(T& a, T& b)
-		{
-			return (a < b ? a : b);
-		}
 
-		static T& max(T& a, T& b)
-		{
-			return (a > b ? a : b);
-		}
+/* ================================ Min/Max ================================ */
 
-		static const T& min(const T& a, const T& b)
-		{
-			return (a < b ? a : b);
-		}
+template <typename T>
+T& min(T& a, T& b)
+{
+	return (a < b ? a : b);
+}
 
-		static const T& max(const T& a, const T& b)
-		{
-			return (a > b ? a : b);
-		}
-};
+template <typename T>
+T& max(T& a, T& b)
+{
+	return (a > b ? a : b);
+}
+
+
+/* ============================= Const Min/Max ============================= */
+
+template <typename T>
+const T& min(const T& a, const T& b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+const T& max(const T& a, const T& b)
+{
+	return (a > b ? a : b);
+}
+
+
+
+/* ============================ Class Template ============================= */
+
+// template <typename T>
+// class Whatever
+// {
+// 	public:
+// 		static void swap(T& a, T& b)
+// 		{
+// 			T temp = a;
+// 			a = b;
+// 			b = temp;
+// 		}
+
+// 		static T& min(T& a, T& b)
+// 		{
+// 			return (a < b ? a : b);
+// 		}
+
+// 		static T& max(T& a, T& b)
+// 		{
+// 			return (a > b ? a : b);
+// 		}
+
+// 		static const T& min(const T& a, const T& b)
+// 		{
+// 			return (a < b ? a : b);
+// 		}
+
+// 		static const T& max(const T& a, const T& b)
+// 		{
+// 			return (a > b ? a : b);
+// 		}
+// };
 
 #endif
