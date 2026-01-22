3663d55212649079ca6982ec8ccd7084dbc55550
KrispyFbacon <fbrandaoporto@gmail.com>
2026-01-22 18:17:44 +0000

    Cpp09 ex00 start

---
+++ CPP_09/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frbranda <frbranda@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 17:05:40 by frbranda          #+#    #+#             */
+/*   Updated: 2026/01/22 18:17:10 by frbranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//# include ""
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+		std::cout << "Error: 1 args!" <<std::endl;
+	
+	std::cout << "Nice" <<std::endl;
+	return 0;
+}
+
