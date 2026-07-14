46877e798efa0bf66d33763ab6122167ad90760b
KrispyFbacon <fbrandaoporto@gmail.com>
2026-07-14 19:28:47 +0100

    Update sortVector

--- CPP_09/ex02/PmergeMe.cpp
+++ CPP_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: frbranda <frbranda@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 11:47:36 by frbranda          #+#    #+#             */
-/*   Updated: 2026/06/10 18:49:19 by frbranda         ###   ########.fr       */
+/*   Updated: 2026/07/14 18:48:31 by frbranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,30 +85,22 @@ void PmergeMe::sort()
 
 /* ========================== Vector Implematation ========================= */
 
-void PmergeMe::sortVector()
+void PmergeMe::_sortVector()
 {
-	//TODO Make pair fuctions?
-	
-	mergeInsertSort(_vector);
-}
-
-void PmergeMe::mergeInsertSort(std::vector<int>& arr)
-{
-	if (arr.size() <= 1)
+	if (_vector.size() <= 1)
 		return ;
 	
 	// TODO 1) Pairs
-	
 	std::vector<std::pair<int, int>> pairs;
 
 	// Reserves memory for vectors for performance and no re-size needed!
-	size_t halfSize = arr.size() / 2;
+	size_t halfSize = _vector.size() / 2;
 	pairs.reserve(halfSize);
 
 	for (size_t i = 0; i < halfSize; ++i)
 	{
-		int a = arr[2 * i];
-		int b = arr[2 * i + 1];
+		int a = _vector[2 * i];
+		int b = _vector[2 * i + 1];
 		
 		if (a > b)
 			pairs.push_back(std::make_pair(a, b));
@@ -116,52 +108,86 @@ void PmergeMe::mergeInsertSort(std::vector<int>& arr)
 			pairs.push_back(std::make_pair(b, a));
 	}
 	
-	mergeInsertSortPairs(pairs);
+	// TODO 2) Winner sort
+	_mergeSortPairs(pairs, 0, pairs.size());
 	
-	// TODO 2) Main 
-	std::vector<int> winners;
-	std::vector<int> losers;
-	
-	winners.reserve(arr.size());
-	losers.reserve(halfSize);
-
-	
-	// TODO 3) Pend
-	// TODO 4) Binary search 
-	// TODO 5) Jacobsthal order
-
-	
-	if (arr.size() % 2 != 0)
-	{
-		// Insert leftover
-		//arr.back();
-	}
-}
-
-// TODO - Instead use int pair vector recursion
-void PmergeMe::mergeInsertSortPairs(std::vector<Pair>& pairs)
-{
-	//TODO EVERYTHING WRONG
-	if (pairs.size() <= 1)
-		return ;
-
-	std::vector<std::pair<int, int>> winnerPairs;
-
-	size_t	halfSize = pairs.size() / 2;
-	winnerPairs.reserve(halfSize);
+	// TODO 3) Binary search 
+	std::vector<int> mainChain;
 
 	for (size_t i = 0; i < halfSize; ++i)
-	{
-		int a = pairs[2 * i].first;
-		int b = pairs[2 * i + 1].first;
+		mainChain.push_back(pairs[i].first);
 
-		if (a > b)
-			winnerPairs()
-	}
+
+ 	// TODO 4) Jacobsthal order
+	std::vector<size_t> jacob = generateJacobsthal(halfSize);
 	
+	size_t lastIdx = 1;
+	for (size_t i; i < jacob.size(); ++i)
+	{
+		size_t current = std::min(jacob[i], halfSize);
+		
+		for (size_t j = current; j >= lastIdx; --j)
+		{
+			_binaryInsert();
+		}
 
+		lastIdx = current;
+	}
+
+	//if (arr.size() % 2 != 0)
+	// 	{
+	// 		// Insert leftover
+	// 		//arr.back();
+	// 	}
 }
 
+void PmergeMe::_mergeSortPairs(std::vector<Pair>& pairs, size_t left, size_t right)
+{
+	if (right - left <= 1)
+		return;
+
+	size_t mid = left + (right - left) / 2;
+
+	_mergeSortPairs(pairs, left, mid);
+	_mergeSortPairs(pairs, mid, right);
+	_mergePairs(pairs, left, mid, right);
+}
+
+void PmergeMe::_mergePairs(std::vector<Pair>& pairs, size_t left, size_t mid, size_t right)
+{
+	std::vector<Pair> temp;
+	pairs.reserve(right - left);
+
+	size_t i = left;
+	size_t j = mid;
+
+	while ( i < mid && j < right )
+	{
+		if (pairs[i].first < pairs[j].first)
+			temp.push_back(pairs[i++]);
+		else 
+			temp.push_back(pairs[j++]);
+	}
+
+	while (i < mid)
+		temp.push_back(pairs[i++]);
+	while (j < right)
+		temp.push_back(pairs[j++]);
+
+	for (size_t k = 0; k < temp.size(); ++i)
+		pairs[left + k] = temp[k];
+}
+
+void PmergeMe::_binaryInsert(std::vector<int> &arr,int value, size_t end)
+{
+	size_t pos = _binarySearch(arr, value, end);
+	arr.insert(arr.begin() + pos, value);
+}
+
+size_t PmergeMe::_binarySearch(const std::vector<int> &arr, int value, size_t end)
+{
+	
+}
 
 /* ========================== Deque Implematation ========================== */
 
@@ -169,4 +195,26 @@ void PmergeMe::sortDeque()
 {
 	if (_deque.size() <=1 )
 		return ;
+}
+
+std::vector<size_t> PmergeMe::generateJacobsthal(size_t max)
+{
+	std::vector<size_t> jacob;
+
+	if (max <= 0)
+		return jacob;
+
+	jacob.push_back(1);
+	if (max < 3)
+		return jacob;
+
+	jacob.push_back(3);
+
+	while (jacob.back() < max)
+	{
+		size_t n = jacob.size();
+		jacob.push_back(jacob[n - 1] + 2 * jacob[n - 2]);
+	}
+
+	return jacob;
 }
--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: frbranda <frbranda@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 11:47:28 by frbranda          #+#    #+#             */
-/*   Updated: 2026/06/10 18:29:09 by frbranda         ###   ########.fr       */
+/*   Updated: 2026/07/14 18:48:29 by frbranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,22 @@ class PmergeMe
 		typedef std::vector<Pair>::const_iterator	pairVectorIt;
 		typedef std::deque<Pair>::const_iterator	pairDequeIt;
 		
-		
 		std::vector<int> _vector;
 		std::deque<int> _deque;
 		
 		// Vector Implematation
-		void sortVector();
-		//TODO 
-		void mergeInsertSort(std::vector<int>& arr);
-		void mergeInsertSortPairs(std::vector<Pair>& pairs);
+		void	_sortVector();
+		void	_mergeSortPairs(std::vector<Pair>& pairs, size_t left, size_t right);
+		void	_mergePairs(std::vector<Pair>& pairs, size_t left, size_t mid, size_t right);
+		void	_binaryInsert(std::vector<int> &arr,int value, size_t end);
+		size_t	_binarySearch(const std::vector<int> &arr, int value, size_t end);
 		
 		// Deque Implematation
 		void sortDeque();
 		void mergeInsertSort(std::deque<int>& arr);
+
+		// Jacobsthal number sequence
+		std::vector<size_t> generateJacobsthal(size_t max);
 
 		PmergeMe();
 		PmergeMe(const PmergeMe& other);
